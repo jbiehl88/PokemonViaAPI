f2754d23d113a57156664352f36f37842f1c4c2d
Jordan <99503935+jbiehl88@users.noreply.github.com>
2025-02-18 15:05:05 -0600

    small cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Box } from "@mui/material";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
